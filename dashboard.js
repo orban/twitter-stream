c2e42cc5c3dfcc7e058dffda3dffefcb9192d47b
Ryan Orban <ryan@ryanorban.com>
2012-07-26 16:28:44 -0700

    Wrapped express and socket.io in httpServer()

--- dashboard.js
+++ dashboard.js
@@ -1,20 +1,27 @@
-sys     = require('util');
-express = require('express');
-twitter = require('ntwitter');
+var sys     = require('util'),	
+	express = require('express'),
+	twitter = require('ntwitter'),
+	http	= require('http');
 
-app = express.createServer();
+var app = express();
+var server = http.createServer(app);
+
+
 app.configure(function(){
   app.use(express.static(__dirname + '/public'));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.get('/', function(req, res, next){
   res.render('/public/index.html');
 });
-app.listen(8081);
+
 console.log('Server running at http://localhost:8081/');
 
-var io  = require('socket.io').listen(app);
+//var io  = require('socket.io').listen(app);
+var io = require('socket.io').listen(server);
 io.set('log level', 1);
+server.listen(8081);
 
 myList = [];
 Array.prototype.del = function(val) {
@@ -40,7 +47,7 @@ io.sockets.on('connection', function(socket) {
         socket.emit('pushfilter', myList);
     });
     if(myList.length!=0) {
-        twit.stream('user',{track:myList}, function(stream) {
+        twit.stream('statuses/filter',{track:myList}, function(stream) {
             stream.on('data', function (tweet) {
   	    	    socket.emit('message', JSON.stringify(tweet));
             });
@@ -50,9 +57,9 @@ io.sockets.on('connection', function(socket) {
 
 function CreateTwitter() {
 twit = new twitter({
-    consumer_key: '',
-    consumer_secret: '',
-    access_token_key: '',
-    access_token_secret: ''
+	consumer_key: 'grCZGQ4aNUJpuegc18Zvng',
+    consumer_secret: 'lyfXpkspgMLcwURhm6JMnvcft4eIV999ECyTW9cgdKY',
+    access_token_key: '709829016-ZVHfzyXPfttrahyGwUAOasWK7wDNE2KihL4mPJ6t',
+    access_token_secret: 'Cs5eyq3ZIakPsrObvmbCNdLP1cxx5XmGwCic8gTgc'
 });
 }
